31e35521a3bb3add74e2304df9171452382bcd53
ET-CS <et.programming@gmail.com>
2013-08-20 10:06:24 +0300

    remove unused lines

--- md5gen.sh
+++ md5gen.sh
@@ -1,2 +1,1 @@
-#find . -type f \( ! -iname "md5.txt" \) -print0 | xargs -0 md5sum > md5.txt
 find . -type f ! -name 'md5.txt' -print0 | xargs -0 md5sum > md5.txt
--- package.sh
+++ package.sh
@@ -1,2 +1,1 @@
-git archive --format=tar HEAD | gzip > erpxe${VERSION}.tar.gz
-#git archive --format=tar --prefix=mahara-${VERSION}/ ${RELEASETAG} | bzip2 -9 > ${CURRENTDIR}/mahara-${RELEASE}.tar.bz2
+git archive --format=tar HEAD | gzip > erpxe.tar.gz
