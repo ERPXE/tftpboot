e59d88e1715ea03d5d24f0a91ae90c71f652a737
RaveMaker <ravemaker@msn.com>
2013-08-24 14:14:34 +0300

    BIN folder

---
+++ bin/fix-permissions.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Fix files and folders ownership and permissions"
+echo ""
+echo "Set ownership to user root and group root"
+chown root:root /tftpboot/ -R
+echo "Set files permissions 644"
+chmod 644 /tftpboot/ -R
+echo "Set folder permissions 755"
+find /tftpboot/ -type d -print | xargs chmod 755
+echo "Reset script permissions"
+chmod 755 /tftpboot/bin/fix-permissions.sh /tftpboot/bin/package.sh
+echo ""
+echo "Done"
