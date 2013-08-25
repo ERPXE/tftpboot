6d8f504b357210bf121f58f842e254f3055acf40
ET-CS <et.programming@gmail.com>
2013-08-25 17:52:19 +0300

    update echos

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 filename="erpxe-nightly.tar.gz";
+echo "packaging branch to $filename.";
 git archive --format=tar HEAD | gzip > $filename;
-echo package $filename created.
+echo "package $filename created successfully.";
