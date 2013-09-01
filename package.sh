5536a4fad0e8369db032fb712403cbc073d84afd
ET-CS <et.programming@gmail.com>
2013-09-02 00:05:39 +0300

    Instructions before package

--- package.sh
+++ package.sh
@@ -1,6 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
-filename="erpxe-nightly.tar.gz";
-echo "packaging branch to $filename.";
-git archive --format=tar HEAD | gzip > $filename;
-echo "package $filename created successfully.";
+FILE="BEFORE_PACKAGING"
+cat $FILE
+echo
+
+read -r -p "Did you followed instructions? [Y/n] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        filename="erpxe-nightly.tar.gz";
+        echo "packaging branch to $filename.";
+        git archive --format=tar HEAD | gzip > $filename;
+        echo "package $filename created successfully.";
+        ;;
+    *)
+        echo "I am waiting till you done.";
+        ;;
+esac
+
+
