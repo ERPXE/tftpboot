b917422c87c09243240b2c8303dfdba95b5ba263
ET-CS <et.programming@gmail.com>
2013-09-10 04:24:25 +0300

    links deleted for all the subnet files (recreate using script)

--- bin/create_links.sh
+++ bin/create_links.sh
@@ -8,7 +8,24 @@ function ln_boot() {
     ln -s boot/isolinux/$1 ./$1
 }
 
+function ln_pxe() {
+    ln -s default ./$1
+}
+
 # create root links to .c32 files.
 ln_boot ldlinux.c32
 ln_boot libcom32.c32
 ln_boot libutil.c32
+
+# create common subnets loaders in pxelinux.cfg
+cd pxelinux.cfg
+ln_pxe 0A
+ln_pxe 0A0
+ln_pxe 0A00
+ln_pxe 0A000
+ln_pxe 0A0000
+ln_pxe C0
+ln_pxe C0A
+ln_pxe C0A8
+ln_pxe C0A80
+ln_pxe C0A800
