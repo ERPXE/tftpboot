4e12f977bd93a9f3645608aac90a21cd310fc2e6
ET-CS <et.programming@gmail.com>
2013-09-10 05:32:47 +0300

    renamed files

---
+++ bin/package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ..
+
+FILE="PREPACK"
+cat $FILE
+echo
+
+read -r -p "Did you followed instructions? [Y/n] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        filename="erpxe-nightly.tar.gz";
+        echo "packaging branch to $filename.";
+        git archive --format=tar HEAD | gzip > bin/$filename;
+        echo "package $filename created successfully.";
+        ;;
+    *)
+        echo "I am waiting till you done.";
+        ;;
+esac
+
+
