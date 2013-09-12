589d58207a80aa760c8dcb36c71b9648a7b12c7e
RaveMaker <ravemaker@gmail.com>
2013-09-12 13:16:49 +0300

    Updated script to v0.2

--- bin/reset_permissions.sh
+++ bin/reset_permissions.sh
@@ -10,11 +10,10 @@ function resetPermissions() {
     chown root:root $FOLDER/ -R
     echo "Set files permissions 644"
     chmod 644 $FOLDER/ -R
+    echo "Reset script permissions"
+    chmod +x $FOLDER/bin/*.sh
     echo "Set folder permissions 755"
     find $FOLDER/ -type d -print | xargs chmod 755
-    echo "Reset script permissions"
-    chmod 744 $FOLDER/bin/ -R
-    chmod +x $FOLDER/*.sh
 }
 
 path="/tftpboot/";
