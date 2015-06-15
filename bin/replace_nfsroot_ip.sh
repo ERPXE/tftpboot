ada2709b28a1c03552d66eb56e6a5797bb116115
Espen <eoyslebo@gmail.com>
2015-06-15 20:06:35 +0200

    fixed replace_nfsroot_ip.sh for OpenELEC plugin

--- bin/replace_nfsroot_ip.sh
+++ bin/replace_nfsroot_ip.sh
@@ -11,4 +11,5 @@ find /tftpboot -type f |grep '\.menu$' | while read file
 do
 echo "$file"
 sed "-i.`date +%F-%H%M%S`.bak" -r  's/nfsroot=(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/nfsroot=$IPAddress/ "$file"
+sed "-i.`date +%F-%H%M%S`.bak" -r  's/NFS=(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b'/NFS=$IPAddress/g "$file"
 done
